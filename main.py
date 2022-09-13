9717e3a60cd4efbc43a43a0a2d5364c23f669b48
BrianC15 <109993220+H0waru@users.noreply.github.com>
2022-09-13 08:27:44 +0000

    Finished making the intro and add imports

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-#Menu function 
+#import
+import time
+
+
+#Menu function
 def menu():
     print("*" * 27)
     print("* (1) Flat white    $3.00 *")
@@ -7,3 +11,13 @@ def menu():
     print("* (4) Decaf         $3.00 *")
     print("* (5) Hot Chocolate $4.00 *")
     print("*" * 27)
+
+
+#introduction
+def intro():
+    print("*-" * 25)
+    print("Welcome to CCC Cafe".center(50))
+    print("*-" * 25)
+    time.sleep(2)
+    print("\nPlease have a look at our menu:")
+    menu()
