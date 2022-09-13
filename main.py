c11a3c756abb5981920f9a7c1abd89b90ffa9300
BrianC15 <109993220+H0waru@users.noreply.github.com>
2022-09-13 08:25:31 +0000

    Created a menu function

---
+++ main.py
@@ -0,0 +1,9 @@
+#Menu function 
+def menu():
+    print("*" * 27)
+    print("* (1) Flat white    $3.00 *")
+    print("* (2) Cappaccino    $3.00 *")
+    print("* (3) Latte         $3.50 *")
+    print("* (4) Decaf         $3.00 *")
+    print("* (5) Hot Chocolate $4.00 *")
+    print("*" * 27)
